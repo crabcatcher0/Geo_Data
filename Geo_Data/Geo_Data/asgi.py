f5fb49dde04982414bca732c1023327f5e083476
crabcatcher0 <rautprashant103@gmail.com>
2024-10-18 19:43:47 +0545

    initial

--- Geo_Data/Geo_Data/asgi.py
+++ Geo_Data/Geo_Data/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Geo_Data.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Geo_Data.settings")
 
 application = get_asgi_application()
