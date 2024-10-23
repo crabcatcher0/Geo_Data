f66cf90c301d3ebba6a557be600fe0e521d6fb3d
crabcatcher0 <rautprashant103@gmail.com>
2024-10-23 23:06:25 +0545

    feat: testing library

--- Geo_Data/core/models.py
+++ Geo_Data/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class City(models.Model):
@@ -18,3 +19,18 @@ class Hospital(models.Model):
 
     def __str__(self):
         return self.hospital_name
+
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=100)
+    author_name = models.CharField()
+    available_status = models.BooleanField(default=False)
+    lost_status = models.BooleanField(default=False)
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    total_amount = models.FloatField()
